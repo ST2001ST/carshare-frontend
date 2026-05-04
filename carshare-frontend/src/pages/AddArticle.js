a49640ef10628d4a37497027edcf7b217cfccc3a
ST2001ST <touzanisouhaila10@gmail.com>
2026-05-04 02:54:21 +0100

    Update AddArticle.js

--- carshare-frontend/src/pages/AddArticle.js
+++ carshare-frontend/src/pages/AddArticle.js
@@ -29,7 +29,7 @@ function AddArticle() {
     }
 
     try {
-      await axios.post('http://localhost:5000/api/voitures', {
+      await axios.post(`${process.env.REACT_APP_API_URL}/api/voitures`, {
         marque,
         modele,
         matricule: matricule || '',
