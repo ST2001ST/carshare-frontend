56b765b4361ed9ed7389d33d84899838eaa4df7b
ST2001ST <touzanisouhaila10@gmail.com>
2026-05-05 00:36:17 +0100

    Fix formatting and remove unnecessary lines in db.js

--- carshare-api/db.js
+++ carshare-api/db.js
@@ -5,8 +5,7 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    charset: 'utf8mb4'
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
