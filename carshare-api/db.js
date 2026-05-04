f777869543755719e45d9e12027adea94969a617
ST2001ST <touzanisouhaila10@gmail.com>
2026-05-04 03:00:38 +0100

    Refactor database connection to use environment variables

--- carshare-api/db.js
+++ carshare-api/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root@2026',
-    database: 'gestion_produits'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: 'utf8mb4'
 });
 
 connection.connect((err) => {
